6b8f640e66618b7a50542d69f407bcd7736d6ebe
Ayesh Alshukri <aalshukri@users.noreply.github.com>
2020-09-06 17:03:32 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -99,9 +99,9 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 #
 git add . && \
 git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-#git push --force $remote_repo master:$remote_branch && \
+git push --force $remote_repo master:$remote_branch && \
 #remove force
-git push $remote_repo master:$remote_branch && \
+#git push $remote_repo master:$remote_branch && \
 rm -fr .git && \
 cd .. 
 
