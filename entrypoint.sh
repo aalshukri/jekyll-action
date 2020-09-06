0110b7ac08bead3402252dbfc3ee2dc09016a759
Ayesh Alshukri <aalshukri@users.noreply.github.com>
2020-09-06 16:19:47 +0100

    test update

--- entrypoint.sh
+++ entrypoint.sh
@@ -96,8 +96,6 @@ remote_repo="https://${JEKYLL_PAT}@github.com/${GITHUB_REPOSITORY}.git" && \
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-#added pull
-git pull $remote_repo master:$remote_branch && \
 git add . && \
 git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
 #git push --force $remote_repo master:$remote_branch && \
